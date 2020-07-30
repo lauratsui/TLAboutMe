d9ec3650bc92f178a1edec7b03aa45c6cd3f9881
lauratsui <lauratsui93@gmail.com>
2020-07-30 18:10:39 +0800

    New About Me page for desktop

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <the-desktop-nav id="desktop-nav" />
+    <the-mobile-nav id="mobile-nav" />
+     <div class="view-container">
+       <router-view/>      
+     </div>
+  </div>
+</template>
+
+<script>
+  import TheDesktopNav from '@/components/TheDesktopNav'
+  import TheMobileNav from '@/components/TheMobileNav' 
+
+  export default {
+    components: {
+      TheDesktopNav,
+      TheMobileNav
+    }  
+  }
+</script>
+
+<style lang="sass" scoped>
+  .view-container
+    width: 79vw
+    padding: 1.5em
+    margin-left: 20vw
+    box-sizing: border-box 
+    min-height: 100vh  
+</style>
---
+++ src/components/TheMobileNav.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="nav">
+        <div class="nav-logo">
+            <img src="https://img.favpng.com/14/12/13/cat-icon-design-icon-png-favpng-0hmD7vw79EmtTwWMXUSAgXZWH.jpg" alt="logo" class="logo" height="75" width="75">
+        </div>
+        <div class="nav-menu">
+            <ul class="nav-menu-list">
+                <li class="nav-list-item">About Me</li>
+                <li class="nav-list-item">Github</li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="sass" scoped>
+    .nav
+        background: #333
+        display: flex
+        flex-direction: column
+        justify-content: space-around
+        align-items: center
+        .nav-logo
+            margin: 20px auto
+
+        .nav-menu
+            width: 100% 
+</style>
